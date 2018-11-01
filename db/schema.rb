5beb46c6f719e5f866389c024a6013519ef3bb75
john <john.ouseph@navigantbpm.com>
2018-11-01 17:22:20 +0530

    add password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_041854) do
+ActiveRecord::Schema.define(version: 2018_11_01_114250) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_31_041854) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
