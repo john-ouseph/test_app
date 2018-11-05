4ed77c185f1bc38dc68ccb6a4781e28e8b288b2f
john <john.ouseph@navigantbpm.com>
2018-11-05 18:02:58 +0530

    admin user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_104417) do
+ActiveRecord::Schema.define(version: 2018_11_05_113356) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_11_02_104417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
